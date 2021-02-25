b87d9462a223dabdb5323a99c27661b59c1216dd
Alex Kushleyev <drone-guy>
2021-02-24 23:40:56 -0500

    improve versatility of firmware upload script

--- tools/voxl-esc-upload-firmware.py
+++ tools/voxl-esc-upload-firmware.py
@@ -49,6 +49,8 @@ parser.add_argument('--firmware-baud-rate',   required=False, default=None)
 parser.add_argument('--bootloader-baud-rate', type=int, required=False, default=230400)
 parser.add_argument('--firmware-file',        type=str, required=True,  default="")
 parser.add_argument('--id',                   type=int, required=True,  default=0)
+parser.add_argument('--skip-firmware-reset',  type=str, required=False, default="false")
+parser.add_argument('--progress-bar-type',    type=str, required=False, default="fancy")
 args = parser.parse_args()
 
 devpath              = args.device
@@ -56,16 +58,6 @@ firmware_baud_rate   = args.firmware_baud_rate
 bootloader_baud_rate = args.bootloader_baud_rate
 firmware_file        = args.firmware_file
 esc_id               = args.id
-
-# quick scan for ESCs to detect the port
-scanner = EscScanner()
-(devpath, firmware_baud_rate) = scanner.scan(devpath, firmware_baud_rate)
-
-if devpath is not None and firmware_baud_rate is not None:
-    print('INFO: ESC(s) detected on port: ' + devpath + ', baud rate: ' + str(firmware_baud_rate))
-else:
-    print('ERROR: No ESC(s) detected, exiting.')
-    sys.exit(1)
 
 # load file
 f = open(firmware_file, "rb")
@@ -76,50 +68,80 @@ try:
         firmware_binary.append(ord(byte))
         byte = f.read(1)
 except:
-    print 'ERROR: Unable to open firmware file %s' % (firmware_file)
+    print('ERROR: Unable to open firmware file %s' % (firmware_file))
     sys.exit(1)
 finally:
     f.close()
 
-# reset the required ESC so that it can enter bootloader
 esc_manager = EscManager()
-if firmware_baud_rate is not None:
-    try:
-        esc_manager.open(devpath, firmware_baud_rate)
-    except Exception as e:
-        print 'WARNING: Unable to detect ESCs in order to reset them..:'
-        print e
-        print 'WARGING: Attempting to continue'
 
-        #since we did not find ESCs with firmware, we can't send the reset message
-        #so, just try to establish communication with bootloader
-        esc_manager.open(devpath, bootloader_baud_rate)
+if "true" not in args.skip_firmware_reset.lower():
+    # quick scan for ESCs to detect the port
+    scanner = EscScanner()
+    (devpath, firmware_baud_rate) = scanner.scan(devpath, firmware_baud_rate)
+
+    if devpath is not None and firmware_baud_rate is not None:
+        print('INFO: ESC(s) detected on port: ' + devpath + ', baud rate: ' + str(firmware_baud_rate))
+    else:
+        print('ERROR: No ESC(s) detected, exiting.')
+        sys.exit(1)
+
+    # reset the required ESC so that it can enter bootloader
+    if firmware_baud_rate is not None:
+        try:
+            esc_manager.open(devpath, firmware_baud_rate)
+            time.sleep(0.25)
+            escs = esc_manager.get_escs()
+            esc_ids = [e.get_id() for e in escs]
+            if not esc_id in esc_ids:
+                print('WARNING: Specified ESC ID not detected; perform manual power cycle now')
+            else:
+                print('INFO: Sending reset command to ESC ID %d' % esc_id)
+                esc_manager.reset_id(esc_id)
+                time.sleep(0.25)
+        except Exception as e:
+            print('WARNING: Unable to detect ESCs in order to reset them..:')
+            print(e)
+            print('WARGING: Attempting to continue')
+
+            #since we did not find ESCs with firmware, we can't send the reset message
+            #so, just try to establish communication with bootloader
+            esc_manager.open(devpath, bootloader_baud_rate)
+
 else:
     esc_manager.open(devpath, bootloader_baud_rate)
 
-time.sleep(0.25)
-escs = esc_manager.get_escs()
-esc_ids = [e.get_id() for e in escs]
-if not esc_id in esc_ids:
-    print 'WARNING: Specified ESC ID not detected; perform manual power cycle now'
-else:
-    esc_manager.reset_id(esc_id)
-    time.sleep(0.25)
 
 # switch to bootloader protocol
 esc_manager.set_protocol(types.ESC_PROTOCOL_BOOTLOADER)
 esc_manager.set_baudrate(bootloader_baud_rate)
 
+print('INFO: Installing firmware to ESC ID %d : %s' % (esc_id,firmware_file))
 print('')
+progress_bar_length = 50
+if args.progress_bar_type == 'simple':
+    simple_progress_bar_current_length = 0
+    print('\tProgress:  0' + '.'*(progress_bar_length-5) + '100')
+    print('\t          '),
+
+
 for progress in esc_manager.upload_firmware(firmware_binary, esc_id):
     if progress == -1:
-        print 'ERROR: An error occured during the write process.'
+        print('ERROR: An error occured during the write process.')
         sys.exit(1)
     else:
-        bar_length = 50
-        bar_completed_length = int(bar_length*progress)
-        progress_bar = '#' * bar_completed_length + ' ' * (bar_length-bar_completed_length)
-        print('\033[FProgress: %3d%% [%s]' % ((100 * progress),progress_bar))
+        if args.progress_bar_type == 'fancy':
+            bar_completed_length = int(progress_bar_length*progress)
+            progress_bar = '#' * bar_completed_length + ' ' * (progress_bar_length-bar_completed_length)
+            print('\033[F\tProgress: %3d%% [%s]' % ((100 * progress),progress_bar))
+        elif args.progress_bar_type == 'simple':
+            bar_completed_length = int(progress_bar_length/2*progress)
+            if bar_completed_length > simple_progress_bar_current_length:
+                print('#'),
+                sys.stdout.flush()
+                simple_progress_bar_current_length += 1
+
 
 esc_manager.close()
-print 'INFO: Firmware successfully updated for ESC id %d' % (esc_id)
+print('')
+print('INFO: Firmware successfully updated for ESC id %d' % (esc_id))
